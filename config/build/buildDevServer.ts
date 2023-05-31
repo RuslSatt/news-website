2a6bcd631277175d86cdc80e22faa8addd26f270
Ruslan Sattarov <r8sattarov@mail.ru>
2023-05-31 21:56:53 +0500

    feat: add Routes and Suspense (Lazy)

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,7 @@ import {Configuration as ConfigurationDev} from 'webpack-dev-server';
 export function buildDevServer(options: BuildOptions): ConfigurationDev {
     return {
         port: options.port,
-        open: true
+        open: true,
+        historyApiFallback: true,
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import {Counter} from "./components/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Suspense} from "react";
 
 const App = () => {
   return (
       <div className="app">
-        <Counter/>
+          <Link to={'/'}>Главная</Link>
+          <Link to={'/about'}>О странице</Link>
+          <Suspense fallback={'Loading...'}>
+              <Routes>
+                <Route path={'/'} element={<MainPageAsync/>}/>
+                <Route path={'/about'} element={<AboutPageAsync/>}/>
+              </Routes>
+          </Suspense>
       </div>
   )
 }
