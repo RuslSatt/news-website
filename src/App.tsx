f06d3f56375a7ddbff233f9f9584f992e76acb57
Ruslan Sattarov <r8sattarov@mail.ru>
2023-06-01 21:02:47 +0500

    feat: add toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from "react";
+import {useTheme} from "./theme/UseTheme";
 
 const App = () => {
-  return (
-      <div className="app">
+
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+      <div className={`app ${theme}`}>
+          <button onClick={toggleTheme}>
+            Сменить тему
+          </button>
           <Link to={'/'}>Главная</Link>
           <Link to={'/about'}>О странице</Link>
           <Suspense fallback={'Loading...'}>
